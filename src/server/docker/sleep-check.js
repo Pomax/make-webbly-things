d7b7d00c5455250bd93b0b34f7e6ab35b24c613a
Pomax <pomax@nihongoresources.com>
2025-08-31 22:03:30 -0700

    .

--- src/server/docker/sleep-check.js
+++ src/server/docker/sleep-check.js
@@ -1,18 +1,40 @@
-import { getProject } from "../database/project.js";
+import { getProject, loadSettingsForProject } from "../database/project.js";
 import { getAllRunningContainers, stopContainer } from "./docker-helpers.js";
-
 const { max } = Math;
+
+const runInterval = 5;
 const msPerMinute = 60000;
-const thresholdMinutes = 5;
-const threshold = thresholdMinutes * msPerMinute;
 
-const tzo = new Date().getTimezoneOffset();
-const tzp = 100 * (tzo / 60) + (tzo % 60);
-const TZ = `${tzo > 0 ? `-` : `+`}${`${tzp}`.padStart(4, `0`)}`;
+// static projects get 5 minutes:
+const staticThreshold = 5 * msPerMinute;
 
+// docker projects get a day:
+const dockerThreshold = 24 * 60 * staticThreshold;
+
+// date formatting helper
 function date(offset = 0) {
   const d = new Date(Date.now() + offset).toISOString();
   return d.replace(`T`, ` `).replace(`Z`, ``).replace(/\.\d+/, ``);
+}
+
+// logging helper
+function log(msg) {
+  console.log(`[${date()}] ${msg}.`);
+}
+
+// helper function for getting a project
+function project(name) {
+  try {
+    const p = getProject(name);
+    const s = loadSettingsForProject(p.id);
+    return {
+      updated_at: p.updated_at,
+      app_type: s.app_type,
+    };
+  } catch (e) {
+    // orphaned image? O_o
+    stopContainer(image);
+  }
 }
 
 /**
@@ -21,33 +43,27 @@ function date(offset = 0) {
  * or requested for a while.
  */
 export function scheduleContainerCheck() {
-  console.log(
-    `[${date()}] Checking to see if any containers need to be put to sleep.`
-  );
+  log(`Checking to see if any containers need to be put to sleep`);
 
-  getAllRunningContainers().forEach(({ image, createdAt }) => {
-    let p;
-    try {
-      p = getProject(image);
-    } catch (e) {
-      // orphaned image? O_o
-      return stopContainer(image);
-    }
+  getAllRunningContainers().forEach(({ image, createdAt, p }) => {
+    if (!(p = project(image))) return;
+
+    const created = Date.parse(createdAt);
+    const lastUpdate = Date.parse(p.updated_at + ` +0000`);
+    const diff = ((Date.now() - max(created, lastUpdate)) / msPerMinute) | 0;
 
     // Only stop a container if it's both "old enough" *and* the last edit
     // to the project is old enough. Because a container could be a week
     // old but if it's last update was 60 seconds ago, keep it alive.
-    const created = Date.parse(createdAt);
-    const lastUpdate = Date.parse(p.updated_at + ` +0000`);
-    const diff = ((Date.now() - max(created, lastUpdate))/ msPerMinute) | 0;
-    if (diff > thresholdMinutes) {
-      console.log(
-        `[${date()}] ${image} was last touched ${diff} minutes ago, stopping container.`
-      );
+
+    const limit = p.app_type === `docker` ? dockerThreshold : staticThreshold;
+    if (diff > limit) {
+      log(`"${image}" was last touched ${diff} min ago, stopping container`);
       stopContainer(image);
     }
   });
 
-  console.log(`[${date()}] Scheduling next container sleep check for ${date(threshold)}`);
-  setTimeout(scheduleContainerCheck, threshold);
+  const timeout = runInterval * msPerMinute;
+  log(`Scheduling next container sleep check for ${date(timeout)}`);
+  setTimeout(scheduleContainerCheck, timeout);
 }
