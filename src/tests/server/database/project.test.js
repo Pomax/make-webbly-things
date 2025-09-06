0bcd1a2ef369553a927677fb07f4569364c0a31e
Pomax <pomax@nihongoresources.com>
2025-09-05 17:22:44 -0700

    user middleware

--- src/tests/server/database/project.test.js
+++ src/tests/server/database/project.test.js
@@ -14,7 +14,7 @@ import { scrubDateTime } from "../../../helpers.js";
 
 import dotenv from "@dotenvx/dotenvx";
 const envPath = resolve(
-  join(import.meta.dirname, `..`, `..`, `..`, `..`, `.env`)
+  join(import.meta.dirname, `..`, `..`, `..`, `..`, `.env`),
 );
 dotenv.config({ quiet: true, path: envPath });
 
