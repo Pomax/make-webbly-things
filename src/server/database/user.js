39f5d5bdc05d1f71ad229503624ccdde1fd78730
Pomax <pomax@nihongoresources.com>
2025-10-14 21:37:20 -0700

    linting

--- src/server/database/user.js
+++ src/server/database/user.js
@@ -100,7 +100,7 @@ function processUserLoginNormally(userObject) {
     const s = getUserSuspensions(user);
     if (s.length) {
       throw new Error(
-        `This user account has been suspended (${s.map((s) => `"${s.reason}"`).join(`, `)})`
+        `This user account has been suspended (${s.map((s) => `"${s.reason}"`).join(`, `)})`,
       );
     }
   }
@@ -397,6 +397,6 @@ export function updateUserProfile(user, profile) {
       name,
       url: linkHrefs[i],
       sort_order: parseFloat(linkOrder[i]),
-    })
+    }),
   );
 }
--- src/server/routing/middleware.js
+++ src/server/routing/middleware.js
@@ -28,7 +28,7 @@ export function nocache(req, res, next) {
   res.setHeader("Surrogate-Control", "no-store");
   res.setHeader(
     "Cache-Control",
-    "no-store, no-cache, must-revalidate, proxy-revalidate"
+    "no-store, no-cache, must-revalidate, proxy-revalidate",
   );
   res.setHeader("Expires", "0");
   next();
@@ -97,8 +97,8 @@ export async function verifyLogin(req, res, next) {
   if (suspensions.length) {
     return next(
       new Error(
-        `This user account has been suspended (${suspensions.map((s) => `"${s.reason}"`).join(`, `)})`
-      )
+        `This user account has been suspended (${suspensions.map((s) => `"${s.reason}"`).join(`, `)})`,
+      ),
     );
   }
   bindUser(req, res, next);
@@ -253,7 +253,7 @@ function parseFileName(req, res, filename) {
   }
   res.locals.filename = filename + suffix;
   const fullPath = (res.locals.fullPath = resolve(
-    join(ROOT_DIR, CONTENT_DIR, projectSlug, filename + suffix)
+    join(ROOT_DIR, CONTENT_DIR, projectSlug, filename + suffix),
   ));
   const apath = resolve(join(CONTENT_DIR, projectSlug));
   const bpath = resolve(fullPath);
