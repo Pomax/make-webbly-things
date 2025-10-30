1440ed181a8ec4787cdf5bb17842521f19b6dcdc
Pomax <pomax@nihongoresources.com>
2025-10-29 22:05:28 -0700

    .

--- setup.js
+++ setup.js
@@ -1,4 +1,18 @@
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
 import * as Utils from "./src/setup/utils.js";
+
+const data = readFileSync(join(Utils.SETUP_ROOT_DIR, `.env`)).toString();
+const entries = Object.fromEntries(
+  data
+    .split(`\n`)
+    .filter(Boolean)
+    .map((e) => e.split(`=`).map((v) => v.trim().replaceAll(`"`, ``)))
+);
+const keys = Object.keys(entries);
+for (const k of keys) {
+  process.env[k] = entries[k];
+}
 
 // Are we on the right version of Node?
 Utils.checkNodeVersion();
