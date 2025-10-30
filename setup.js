4544745882ee0e5f30c4f7af3b32a72237ce5614
Pomax <pomax@nihongoresources.com>
2025-10-29 22:06:05 -0700

    .

--- setup.js
+++ setup.js
@@ -1,17 +1,21 @@
-import { readFileSync } from "node:fs";
+import { existsSync, readFileSync } from "node:fs";
 import { join } from "node:path";
 import * as Utils from "./src/setup/utils.js";
 
-const data = readFileSync(join(Utils.SETUP_ROOT_DIR, `.env`)).toString();
-const entries = Object.fromEntries(
-  data
-    .split(`\n`)
-    .filter(Boolean)
-    .map((e) => e.split(`=`).map((v) => v.trim().replaceAll(`"`, ``)))
-);
-const keys = Object.keys(entries);
-for (const k of keys) {
-  process.env[k] = entries[k];
+// Parse the .env file
+const envFile = join(Utils.SETUP_ROOT_DIR, `.env`);
+if (existsSync(envFile)) {
+  const data = readFileSync(envFile).toString();
+  const entries = Object.fromEntries(
+    data
+      .split(`\n`)
+      .filter(Boolean)
+      .map((e) => e.split(`=`).map((v) => v.trim().replaceAll(`"`, ``)))
+  );
+  const keys = Object.keys(entries);
+  for (const k of keys) {
+    process.env[k] = entries[k];
+  }
 }
 
 // Are we on the right version of Node?
