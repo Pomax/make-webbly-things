e87c08079944159ac6ffd37bf3f7d5898ebffb41
Max Cerrina <mrcerrina@gmail.com>
2025-11-08 16:40:51 -0600

    wip

--- src/client/editor/tab-tracker.js
+++ src/client/editor/tab-tracker.js
@@ -1,16 +1,17 @@
 const TABS_BEFORE_POPUP = 5;
 export class TabTracker {
-  constructor(editor) {
+  constructor(editor, tabsBeforePopup = TABS_BEFORE_POPUP) {
     this.editor = editor;
-    this.lastFiveKeys = [];
+    this.mostRecentKeys = [];
+    this.maxTabCount = tabsBeforePopup;
   }
 
   get full() {
-    return this.lastFiveKeys.length >= TABS_BEFORE_POPUP;
+    return this.mostRecentKeys.length >= this.maxTabCount;
   }
 
   get allTabs() {
-    return this.lastFiveKeys.every((key) => key === 'Tab')
+    return this.mostRecentKeys.every((key) => key === 'Tab')
   }
 
   initialize() {
@@ -22,14 +23,14 @@ export class TabTracker {
 
   showEscapeMessageOnRepeatedTab = (event) => {
     if (this.full) {
-      this.lastFiveKeys.shift();
+      this.mostRecentKeys.shift();
     }
 
-    this.lastFiveKeys.push(event.key);
+    this.mostRecentKeys.push(event.key);
 
     if (this.full && this.allTabs) {
       alert('In order to tab out of the editor, press escape first.');
-      this.lastFiveKeys = [];
+      this.mostRecentKeys = [];
     }
   };
 }
