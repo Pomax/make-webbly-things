3c670ba7d56b3421630fe0dd7cd055094260594a
Pomax <pomax@nihongoresources.com>
2025-11-30 10:07:38 -0800

    don't pollute PR

--- src/server/database/project.js
+++ src/server/database/project.js
@@ -132,9 +132,14 @@ export function deleteProjectForUser(user, project, adminCall) {
       throw new Error(`Not yours, mate`);
     }
   }
-  for (const a of Access.findAll({ project_id: project.id })) {
-    Access.delete(a);
-  }
+  const { name } = project;
+  // console.log(`Deleting access rules for project ${name}...`);
+  const rules = Access.findAll({ project_id: project.id });
+  for (const r of rules) Access.delete(r);
+  // console.log(`Deleting project ${name}...`);
+  Project.delete(project);
+  // console.log(`Deletion complete.`);
+  return name;
 }
 
 /**
