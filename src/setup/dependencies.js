f2c84ba02622fb76c6a03ac1e02e67f42bf65ff8
Pomax <pomax@nihongoresources.com>
2025-12-07 11:59:07 -0800

    .

--- src/setup/dependencies.js
+++ src/setup/dependencies.js
@@ -1,26 +1,22 @@
 import { execSync } from "node:child_process";
 import { checkFor, STDIO } from "./utils.js";
 import { BYPASS_CADDY, BYPASS_DOCKER } from "../helpers.js";
-import { parseEnvironment } from "../parse-environment.js";
 
 /**
  * Verify we have all the tools necessary to run the codebase.
  */
 export function checkDependencies() {
-  parseEnvironment();
-  const { DOCKER_EXECUTABLE: DOCKER } = process.env;
   const missing = [];
   checkForGit(missing);
   BYPASS_CADDY && checkForCaddy(missing);
   checkForSqlite(missing);
-  try {
-    BYPASS_DOCKER ? true : checkForDocker(missing);
-    if (missing.length) {
-      throw new Error(`Missing dependencies: ${missing.join(`, `)}`);
-    }
-  } catch (e) {
+  const dockerRunning = BYPASS_DOCKER ? true : checkForDocker(missing); // has to be last
+  if (missing.length) {
+    throw new Error(`Missing dependencies: ${missing.join(`, `)}`);
+  }
+  if (!dockerRunning) {
     throw new Error(
-      `The ${DOCKER} command is available, but "${DOCKER} ps" threw an error:\n${e.message}`,
+      `The docker command is available, but docker engine is not currently running.`,
     );
   }
 }
@@ -38,11 +34,17 @@ function checkForCaddy(missing) {
  * that running in the background.
  */
 function checkForDocker(missing) {
+  // Note: we can't get env vars from helper.js here, because the env may
+  //       have changed _after_ helpers.js got loaded in, and there is no
+  //       "reload this module" instruction in ESM.
   const { DOCKER_EXECUTABLE: DOCKER } = process.env;
 
   checkFor(DOCKER, missing);
-  execSync(`${DOCKER} ps`, { shell: true, stdio: `inherit` });
-  return true;
+
+  try {
+    execSync(`${DOCKER} ps`, { shell: true, stdio: STDIO });
+    return true;
+  } catch (e) {}
 }
 
 /**
